1aac4f74cff59de641ba4e20d7863a2c77f4fb30
Amiel Martin <amiel@carnesmedia.com>
2010-02-22 13:00:02 -0800

    add the codes

---
+++ jquery_scope.js
@@ -0,0 +1,19 @@
+/*
+ * jquery_scope.js
+ * Amiel Martin
+ * 2010-02-22
+ * 
+ * Allows a bunch of jquery calls to be default scoped to something other than document.
+ */
+
+(function($) {
+    $.fn.as_scope = function(callback) {
+        var self = this, jquery_proxy = function(selector, context) { return self.find(selector, context); };
+        return callback(jquery_proxy);
+    };
+    
+    // this syntax is nice sometimes
+    $.with_scope = function(scope, callback) {
+        return $(scope).as_scope(callback);
+    };
+})(jQuery);
